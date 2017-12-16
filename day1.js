2633de606d6495a37f252d2a21cce690032f5ce6
Elias Lee <elialee@paypal.com>
2017-12-15 17:50:47 -0800

    Add first implementation of Day 1 Part 2

--- day1.js
+++ day1.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 const numberString = fs.readFileSync('./day1.txt', 'utf-8')
 
-const matchingSum = (numStr) => {
+const matchingNextSum = (numStr) => {
   numStr = numStr.slice(0, -1)
   let count = 0
 
@@ -19,4 +19,21 @@ const matchingSum = (numStr) => {
   return count
 }
 
-console.log(matchingSum(numberString))
+console.log(matchingNextSum(numberString))
+
+const matchingAcrossSum = (numStr) => {
+  numStr = numStr.slice(0, -1)
+  let count = 0
+
+  for (let i = 0; i < numStr.length; i++) {
+    let indexOfAcross = (i + (numStr.length / 2)) % numStr.length
+
+    if (numStr[i] === numStr[indexOfAcross]) {
+      count += parseInt(numStr[i])
+    }
+  }
+
+  return count
+}
+
+console.log(matchingAcrossSum(numberString))
